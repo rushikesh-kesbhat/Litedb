7c0713458f700b5c429d3e859cb287756a6376de
Rushikesh-kesbhat-4189-pcccs-Tybsc <rushikeshkesbhat7775@gmail.com>
2025-03-13 15:49:11 +0530

    Intial Project Push

---
+++ src/main/java/db/Litedb/LitedbApplication.java
@@ -0,0 +1,13 @@
+package db.Litedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/db/Litedb/LitedbApplicationTests.java
@@ -0,0 +1,13 @@
+package db.Litedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
